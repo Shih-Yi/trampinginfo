a7cd31e2d6a1709402ce95dc925e5cef0ef42beb
shihyi <brvast@gmail.com>
2021-04-11 22:53:04 +1200

    add sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
+  devise_scope :user do
+    get '/users', to: 'users/registrations#new'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
