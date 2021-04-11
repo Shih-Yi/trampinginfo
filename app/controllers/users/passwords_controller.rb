a6d510b37b8723b1a46bf55e48a13eef74dcb47b
shihyi <brvast@gmail.com>
2021-04-12 00:11:28 +1200

    modify edit view for password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  layout 'login', only: [:new, :edit]
+  layout 'login'
   # GET /resource/password/new
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get '/users', to: 'users/registrations#new'
+    get '/users/password', to: "users/passwords#new"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
