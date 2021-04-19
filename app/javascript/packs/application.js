127a5a484f0d48bf9265e755cc8ae56aa5df4f3a
shihyi <brvast@gmail.com>
2021-04-20 11:33:56 +1200

    remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -14,7 +14,7 @@ import $ from 'jquery'
 window.jQuery = $;
 window.$ = $;
 
-document.addEventListener("turbolinks:load", () => {
+$(function () {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
@@ -32,5 +32,5 @@ import '../stylesheets/users/sessions'
 import "./main"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
