f0f8707e0872ac01c8e0184be3f1dd3b4afd64ab
shihyi <brvast@gmail.com>
2021-04-21 12:43:22 +1200

    add Pagination to page & handleNextPage function

--- app/javascript/bundles/SearchResults/components/SearchResults.jsx
+++ app/javascript/bundles/SearchResults/components/SearchResults.jsx
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
+import Pagination from '../../AppPagination'
 
 class SearchResults extends Component {
   constructor(props) {
     super(props)
     this.state={
-      getData: false,
+      isLoading: false,
       items: [],
       tracks: new Object(),
       map: null,
+      activePage: 1,
+      perPage: 10,
+      totalPages: 0,
+      pageItems: [],
     }
+    this.initMap = this.initMap.bind(this)
+    this.addDataToMap = this.addDataToMap.bind(this)
     this.getSearchResults = this.getSearchResults.bind(this)
     this.boundsChangedResult = this.boundsChangedResult.bind(this)
+    this.handleNextPage = this.handleNextPage.bind(this)
   }
 
-  initMap = (responseJson) => {
+  initMap = () => {
     let map;
-    let datas;
-    let markers;
-    let setTracks = new Object();
 
-    for (let element of responseJson.features) {
-      setTracks[element.properties.OBJECTID] = element
-    }
-    this.setState({ tracks: setTracks })
-
-    map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("info-map"), {
       zoom: 5.5,
       center: { lat: -40.9006, lng: 174.8860 },
     });
     this.setState({ map: map })
+  }
 
-    markers = [];
+  addDataToMap = (responseJson) => {
+    let datas;
+    let markers = [];
+
     datas = this.state.map.data.addGeoJson(responseJson);
     this.state.map.data.setStyle({
       strokeColor: 'green',
@@ -53,7 +57,7 @@ class SearchResults extends Component {
         });
       markers.push(marker);
     }
-    console.log(this)
+
     this.boundsChangedResult(this.state.map, this.state.tracks);
 
     new MarkerClusterer(this.state.map, markers, {
@@ -63,7 +67,9 @@ class SearchResults extends Component {
   }
 
   boundsChangedResult = (map, tracks) => {
+    let { perPage } = this.state;
     let self = this
+
     google.maps.event.addListener(map, 'bounds_changed', function() {
       let bounds = map.getBounds();
       let maxLat = bounds.getNorthEast().lat();
@@ -81,12 +87,16 @@ class SearchResults extends Component {
             tracks[ele].geometry.coordinates[0][0][0] >= Number(minLng) &&
             tracks[ele].geometry.coordinates[0][0][0] <= Number(maxLng)) {
 
-            r[ele] = tracks[ele]
+          r[ele] = tracks[ele]
         }
           return r;
       }, {})
-      self.setState({items: Object.values(filteredObject), getData: true})
-      console.log(Object.keys(filteredObject).length)
+      self.setState({ items: Object.values(filteredObject),
+                      isLoading: true,
+                      activePage: 1,
+                      totalPages: Math.ceil(Object.keys(filteredObject).length/perPage)
+                    })
+      self.handleNextPage(1);
     })
   }
 
@@ -101,34 +111,84 @@ class SearchResults extends Component {
         return response.json();
       })
       .then(result => {
-        this.initMap(result);
-        this.setState({items: result.features, getData: true})
+        let { perPage } = this.state;
+        let setTracks = new Object();
+
+        for (let element of result.features) {
+          setTracks[element.properties.OBJECTID] = element
+        }
+
+        this.setState({ tracks: setTracks,
+                        items: result.features,
+                        isLoading: true,
+                        totalPages: Math.ceil(result.features.length/perPage)
+                      })
+        this.addDataToMap(result);
+        this.handleNextPage(1);
       })
   }
 
   componentDidMount(){
+    this.initMap();
     this.getSearchResults();
   }
 
+  handleNextPage(activePage) {
+    let { perPage } = this.state;
+    let { items, tracks } = this.state;
+    let firstItem = activePage == 1 ? 0 : (activePage - 1) * perPage
+    let lastItem = activePage * perPage
+    let slicedItems = Object.keys(items).slice(firstItem, lastItem).reduce((result, key) => {
+      result[key] = items[key];
+      return result;
+    }, {});
+
+    let sets = new Object();
+    for (const [key, value] of Object.entries(slicedItems)) {
+      sets[value.properties.OBJECTID] = value
+    }
+
+    let filtered = Object.keys(tracks)
+                     .filter(key => Object.keys(sets).includes(key))
+                    .reduce((obj, key) => {
+                      obj[key] = tracks[key];
+                      return obj;
+                    }, {});
+    this.setState({ pageItems: Object.values(filtered), isLoading: true, activePage:  activePage })
+  }
+
   render() {
-    let { getData, items } = this.state;
-    if (getData)
+    let { isLoading, items, pageItems, totalPages } = this.state;
+
+    if (isLoading)
       return(
-        items.map(item => (
-          <div key={item.properties.OBJECTID} className="card mb-4 shadow-sm">
-            <img className="card-img-top" data-src="" src={item.properties.introductionThumbnail} data-holder-rendered="true"></img>
-            <div className="card-body">
-                <p className="card-text">{item.properties.name}</p>
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="btn-group">
-                    <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+        <div>
+          {pageItems.map(item => (
+            <div key={item.properties.OBJECTID} className="card mb-4 shadow-sm">
+              <img className="card-img-top" data-src="" src={item.properties.introductionThumbnail} data-holder-rendered="true"></img>
+              <div className="card-body">
+                  <p className="card-text">{item.properties.name}</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
                   </div>
-                  <small className="text-muted">9 mins</small>
-                </div>
+              </div>
             </div>
-          </div>
-        ))
+          ))}
+
+          {totalPages > 1 &&
+            <div className="pagination-wrap">
+              <Pagination
+                totalPages={totalPages}
+                currentPage={this.state.activePage}
+                onChange={this.handleNextPage}
+              />
+            </div>
+          }
+        </div>
       );
     else
       return(
