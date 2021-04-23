72a8e8d3bf1b877cdb0f9002683fd7f1e1bf7dd6
shihyi <brvast@gmail.com>
2021-04-23 23:37:44 +1200

    rails g controller tracks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'hello_world', to: 'hello_world#index'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: "users/registrations",
@@ -11,8 +13,7 @@ Rails.application.routes.draw do
     get '/users/password', to: "users/passwords#new"
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-
   get 'search', to: 'home#search'
+
+  resources :tracks
 end
