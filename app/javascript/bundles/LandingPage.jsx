91b4d267dd53579678ff2844610dede2965b84c2
shihyi <brvast@gmail.com>
2021-04-26 08:50:02 +1200

    1. set Link obj with searchInput 2. use className

--- app/javascript/bundles/LandingPage.jsx
+++ app/javascript/bundles/LandingPage.jsx
@@ -10,21 +10,21 @@ const LandingPage = (props) => {
   }
 
   return(
-    <div class="container">
-      <div class="row">
-        <div class="col-md-7 banner">
-          <h1><span class="text-red">TracksInfo</span> </h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-7 banner">
+          <h1><span className="text-red">TracksInfo</span> </h1>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-6">
-          <input type="text" name="search" id="autolocation" placeholder="What are you looking for?" class="form-control" value={searchInput} onChange={setInput} />
+      <div className="row">
+        <div className="col-md-6">
+          <input type="text" name="search" id="autolocation" placeholder="What are you looking for?" className="form-control" value={searchInput} onChange={setInput} />
         </div>
       </div>
       <br/><br/>
-      <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-          <Link to={{ pathname: "/search", search: `searchInput=${searchInput}` }}>
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <Link to={{ pathname: "/search", search: `searchInput=${searchInput}` }} className="text-link">
             <Button variant="primary" bsPrefix="btn btn-normal btn-block">
               Search
             </Button>
@@ -34,7 +34,7 @@ const LandingPage = (props) => {
       <br /><hr /><br />
       <div><h3>Tracks</h3></div>
       <br />
-      <div class="row">
+      <div className="row">
       </div>
     </div>
   );
