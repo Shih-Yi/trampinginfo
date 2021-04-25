f0b87364bfc417d5953fe0ab3fd5f53b46ca2633
shihyi <brvast@gmail.com>
2021-04-25 17:02:31 +1200

    use hook to set url params on Link Object

--- app/javascript/bundles/LandingPage.jsx
+++ app/javascript/bundles/LandingPage.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const LandingPage = (props) => {
+  const [ searchInput, setSearchInput ] = useState('')
+
+  const setInput = (e) => {
+    setSearchInput(e.target.value)
+  }
 
   return(
     <div class="container">
@@ -11,13 +18,17 @@ const LandingPage = (props) => {
       </div>
       <div class="row">
         <div class="col-md-6">
-          <input type="text" name="search" id="autolocation" placeholder="What are you looking for?" class="form-control" />
+          <input type="text" name="search" id="autolocation" placeholder="What are you looking for?" class="form-control" value={searchInput} onChange={setInput} />
         </div>
       </div>
       <br/><br/>
       <div class="row">
         <div class="col-md-4 col-md-offset-4">
-          <input type="submit" name="commit" value="Search" class="btn btn-normal btn-block" data-disable-with="Search" />
+          <Link to={{ pathname: "/search", search: `searchInput=${searchInput}` }}>
+            <Button variant="primary" bsPrefix="btn btn-normal btn-block">
+              Search
+            </Button>
+          </Link>
         </div>
       </div>
       <br /><hr /><br />
