844f7d1cbf49a941e66cf3f52397e2435016c474
shihyi <brvast@gmail.com>
2021-04-27 14:41:10 +1200

    1. [package] add 'axios' 2. add axios on Reporting Form

--- app/javascript/bundles/ReactModule/components/ReportingForm.jsx
+++ app/javascript/bundles/ReactModule/components/ReportingForm.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Form, Input } from 'semantic-ui-react'
+import axios from 'axios';
 
-const FormExampleForm = () => (
-  <Form>
-    <Form.Field>
-      <label>Type</label>
-      <input placeholder='type' />
-    </Form.Field>
-    <Form.Field>
-      <label>Position</label>
-      <input placeholder='position' />
-    </Form.Field>
-    <label>Image</label>
-    <input type="file" class="inputfile" id="embedpollfileinput" />
-    <Form.TextArea label='About' placeholder='Tell us more about you...' />
-  </Form>
-)
+class ReportingForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    }
+  }
 
-export default FormExampleForm
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+    })
+  }
+
+  onClickHandler = () => {
+    let formData = new FormData()
+    formData.append("report[avatar]", this.state.selectedFile)
+    console.log(formData)
+    axios.defaults.headers.post['X-CSRF-Token'] = $('meta[name="csrf-token"]').attr('content');
+    const options = {
+      url: '/reports',
+      method: 'POST',
+      data: formData,
+    };
+    axios(options);
+  }
+
+  render () {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Type</label>
+          <input placeholder='type' />
+        </Form.Field>
+        <Form.Field>
+          <label>Position</label>
+          <input placeholder='position' />
+        </Form.Field>
+        <label>Image</label>
+        <Input type="file" className="inputfile" onChange={this.onChangeHandler} id="embedpollfileinput" />
+        <Form.TextArea label='About' placeholder='Tell us more about you...' />
+        <Button type='submit' onClick={this.onClickHandler} >Submit</Button>
+      </Form>
+    )
+  }
+}
+
+export default ReportingForm
