4146de910683c77f7d4ded37f4963bc3320e8528
shihyi <brvast@gmail.com>
2021-05-02 15:37:27 +1200

    add user avatar from facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -48,6 +49,7 @@ class User < ApplicationRecord
     user = User.new(
       email: auth.info.email,
       password: Devise.friendly_token[0, 20],
+      remote_avatar_url: auth.info.image.gsub('http://', 'https://')
     )
     if auth_provider.user != user
       auth_provider.user = user
