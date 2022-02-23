4a957498100e67e57cf6288842f47aeb4777a7f8
Shih-Yi <brvast@gmail.com>
2022-02-23 22:21:25 +1300

    adjust SearchHeader and searchInput width

--- app/javascript/bundles/SearchColumn/components/SearchResults.jsx
+++ app/javascript/bundles/SearchColumn/components/SearchResults.jsx
@@ -289,11 +289,13 @@ class SearchResults extends Component {
     if (isLoading)
       return(
         <div>
-          <Grid>
-            <Grid.Column>
-              <Search id="searchInput" showNoResults={false} onSearchChange={this.inputOnChange} />
-            </Grid.Column>
-          </Grid>
+          <div className='search-header-box'>
+            <Grid>
+              <Grid.Column>
+                <Search id="searchInput" showNoResults={false} onSearchChange={this.inputOnChange} />
+              </Grid.Column>
+            </Grid>
+          </div>
 
           <Grid doubling container stackable columns={3}>
           {pageItems.map(item => (
