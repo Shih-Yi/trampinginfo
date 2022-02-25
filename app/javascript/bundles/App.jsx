89f914a05f79de0d746087caa994584ccef444d2
SC <>
2022-02-25 20:03:22 +1300

    pass tracks props to PopularTracks components

--- app/javascript/bundles/App.jsx
+++ app/javascript/bundles/App.jsx
@@ -3,11 +3,13 @@ import { HashRouter,Route,Switch } from "react-router-dom";
 import LandingPage from "../bundles/LandingPage/components/LandingPage"
 import SearchColumn from "../bundles/SearchColumn/components/SearchColumn"
 
-const App = () =>{
+const App = (props) =>{
+  const { tracks } = props
+
   return(
     <HashRouter>
       <Switch>
-        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/" render={() => <LandingPage tracks={tracks} />} />
         <Route path="/search" component={SearchColumn}/>
         <Route path="/" component={LandingPage}/>
       </Switch>
